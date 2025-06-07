8e61fe7a597c44c9427bbdeb91b81439db41c72f
yosepgx <46633004+yosepgx@users.noreply.github.com>
2025-06-07 11:56:47 -0500

    Generacion de XML para boletas y facturas correcta

--- server/ambienta/ventas_app/views.py
+++ server/ambienta/ventas_app/views.py
@@ -17,6 +17,7 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 import pandas as pd
 from io import BytesIO
+from ajustes_app.models import Dgeneral
 #cuando un pedido se anula si estaba:
 
 
@@ -177,51 +178,197 @@ class GenerarXMLUBLView(APIView):
     def get(self, request, pedido_id):
         try:
             pedido = Pedido.objects.get(id=pedido_id)
+            cliente = pedido.cotizacion.oportunidad.cliente
         except Pedido.DoesNotExist:
             return Response({'error': 'Pedido no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+        
+        try:
+            general = Dgeneral.objects.get(id=1)
+        except Dgeneral.DoesNotExist:
+            return Response({'error': 'Datos de la empresa no encontrados'}, status=status.HTTP_404_NOT_FOUND)
 
         # Raíz
-        factura = ET.Element("Invoice", attrib={
+        documento = ET.Element("Invoice", attrib={
             "xmlns": "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2",
             "xmlns:cac": NSMAP["cac"],
             "xmlns:cbc": NSMAP["cbc"]
         })
+        #version del ubl
+        ET.SubElement(documento, "cbc:UBLVersionID").text = "2.1"
 
-        # ID de factura y fecha
-        ET.SubElement(factura, "cbc:ID").text = f"F001-{pedido.id:08d}"
-        ET.SubElement(factura, "cbc:IssueDate").text = pedido.fecha.strftime("%Y-%m-%d")
+        #version de estructura del documento
+        ET.SubElement(documento, "cbc:CustomizationID").text = "2.0"
 
-        # Datos del proveedor (emisor)
-        supplier_party = ET.SubElement(factura, "cac:AccountingSupplierParty")
-        supplier = ET.SubElement(supplier_party, "cac:Party")
-        ET.SubElement(supplier, "cbc:Name").text = "MI EMPRESA SAC"
+        #Numeracion Serie - Correlativo ID de factura
+        ET.SubElement(documento, "cbc:ID").text = f"{pedido.serie}-{int(pedido.correlativo):08d}"
 
-        # Datos del cliente
-        customer_party = ET.SubElement(factura, "cac:AccountingCustomerParty")
-        customer = ET.SubElement(customer_party, "cac:Party")
-        ET.SubElement(customer, "cbc:Name").text = pedido.cotizacion.oportunidad.cliente.nombre
+        #fecha de emision
+        ET.SubElement(documento, "cbc:IssueDate").text = pedido.fecha.strftime("%Y-%m-%d")
 
-        # Líneas de productos
-        for i, item in enumerate(pedido.detalles.all(), start=1):
-            line = ET.SubElement(factura, "cac:InvoiceLine")
-            ET.SubElement(line, "cbc:ID").text = str(i)
-            ET.SubElement(line, "cbc:InvoicedQuantity", unitCode=item.producto.umedida_sunat).text = str(item.cantidad)
-            ET.SubElement(line, "cbc:LineExtensionAmount", currencyID="PEN").text = f"{item.precio_unitario * item.cantidad:.2f}"
+        #hora de emision
+        ET.SubElement(documento, "cbc:IssueTime").text = pedido.fecha.strftime("%H:%M:%S")
 
-            item_elem = ET.SubElement(line, "cac:Item")
-            ET.SubElement(item_elem, "cbc:Description").text = item.producto.nombre
 
-            price_elem = ET.SubElement(line, "cac:Price")
-            ET.SubElement(price_elem, "cbc:PriceAmount", currencyID="PEN").text = f"{item.precio_unitario:.2f}"
+        invoice_type_code_value = "01" if pedido.tipo_comprobante == Pedido.TIPOFACTURA else "03"
+
+        #Codigo de Tipo de Documento
+        ET.SubElement(documento, 
+            "cbc:InvoiceTypeCode", 
+            attrib={
+                "listAgencyName": "PE:SUNAT",
+                "listName": "SUNAT:Identificador de Tipo de Documento",
+                "listURI": "urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo01"
+            }
+            ).text = invoice_type_code_value
+        
+        #Tipo de moneda
+        ET.SubElement(documento, "cbc:DocumentCurrencyCode",
+                      attrib={
+                          "listID" : "ISO 4217 Alpha" ,
+                          "listName" : "Currency",
+                          "listAgencyName": "United Nations Economic Commission for Europe",
+                      }).text = "PEN"
+        
+        # Datos del emisor (nombre comercial, razonsocial, tipo y numero de ruc)#################
+        supplier_main_party = ET.SubElement(documento, "cac:AccountingSupplierParty")
+        supplier_party = ET.SubElement(supplier_main_party, "cac:Party")
+        supplier_name = ET.SubElement(supplier_party, "cac:PartyName")
+        ET.SubElement(supplier_name, "cbc:Name").text = general.nombre_comercial
+
+        #razon social
+        supplier_tax_scheme = ET.SubElement(supplier_party, "cac:PartyTaxScheme")
+        ET.SubElement(supplier_tax_scheme, "cbc:RegistrationName").text = general.razon_social
+
+        #ruc del emisor
+        ET.SubElement(supplier_tax_scheme, "cbc:CompanyID",
+                       attrib={
+                           "schemeID" : "6",
+                           "schemeName" : "SUNAT:Identificador de Documento de Identidad",
+                           "schemeAgencyName" : "PE:SUNAT" ,
+                           "schemeURI" : "urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo06",
+                       }).text = general.codigo_RUC
+        tax_scheme = ET.SubElement(supplier_tax_scheme, "cac:TaxScheme")
+        ET.SubElement(tax_scheme, "cbc:ID").text = '-'
+
+        #direccion 0000
+        supplier_address = ET.SubElement(supplier_tax_scheme, "cac:RegistrationAddress")
+        ET.SubElement(supplier_address,"cbc:AddressTypeCode").text = '0000'
+
+
+        #Datos del cliente Customer#############
+        # <cac:AccountingCustomerParty>
+        customer_party = ET.SubElement(documento, "cac:AccountingCustomerParty")
+
+        # <cac:Party>
+        party = ET.SubElement(customer_party, "cac:Party")
+
+        # <cac:PartyTaxScheme>
+        party_tax_scheme = ET.SubElement(party, "cac:PartyTaxScheme")
+
+        # <cbc:RegistrationName>
+        ET.SubElement(party_tax_scheme, "cbc:RegistrationName").text = cliente.nombre
+
+        # <cbc:CompanyID ...>
+        customer_document_type = "6" if cliente.tipo_documento == 'RUC' else "1"
+        ET.SubElement(
+            party_tax_scheme,
+            "cbc:CompanyID",
+            attrib={
+                "schemeID": customer_document_type,
+                "schemeName": "SUNAT:Identificador de Documento de Identidad",
+                "schemeAgencyName": "PE:SUNAT",
+                "schemeURI": f"urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo06",
+            }
+        ).text = cliente.documento
+
+        # Relleno ID 
+        tax_scheme = ET.SubElement(party_tax_scheme, "cac:TaxScheme")
+        ET.SubElement(tax_scheme, "cbc:ID").text = "-"
 
         # Total
-        total = ET.SubElement(factura, "cac:LegalMonetaryTotal")
+        total = ET.SubElement(documento, "cac:LegalMonetaryTotal")
         ET.SubElement(total, "cbc:PayableAmount", currencyID="PEN").text = f"{pedido.monto_total:.2f}"
 
-        xml_bytes = ET.tostring(factura, encoding='utf-8', xml_declaration=True)
+        # Líneas de productos cac:InvoiceLine
+        for i, item in enumerate(pedido.detalles.all(), start=1):
+            line = ET.SubElement(documento, "cac:InvoiceLine")
+            ET.SubElement(line, "cbc:ID").text = str(i)
+            ET.SubElement(line, "cbc:InvoicedQuantity", attrib={
+                "unitCode" : item.producto.umedida_sunat,
+            } ).text = str(item.cantidad)
+
+            # si es unitario es al producto si es item es a la linea
+            #valor de venta por item -> subtotal sin igv
+            factor_igv = item.producto.igv
+            descuento_item_sin_igv = item.descuento / (1 + factor_igv)
+            descuento_unitario_con_igv = item.descuento / item.cantidad
+            valor_unitario_sin_igv = item.precio_unitario / (1 + factor_igv) #el valor siempre ya lo guardo sin el descuento aplicado
+
+            descuento_item_con_igv = item.descuento
+            precio_unitario_con_igv = item.precio_unitario
+
+            valor_item_sin_igv = (valor_unitario_sin_igv * item.cantidad) 
+            #subtotal_sin_igv de la linea
+            ET.SubElement(line, "cbc:LineExtensionAmount", currencyID="PEN").text = f"{valor_item_sin_igv - descuento_item_sin_igv :.2f}"
+
+            #precio unitario de venta
+            princing_reference = ET.SubElement(line, "cac:PricingReference")
+            alternative_condition_price = ET.SubElement ( princing_reference, "cac:AlternativeConditionPrice")
+            ET.SubElement(alternative_condition_price, "cbc:PriceAmount", currencyID = "PEN").text = f"{precio_unitario_con_igv - descuento_unitario_con_igv :.2f}"
+            ET.SubElement(alternative_condition_price, "cbc:PriceTypeCode",
+                           attrib={
+                               "listName" : "SUNAT:Indicador de Tipo de Precio",
+                               "listAgencyName" : "PE:SUNAT",
+                               "listURI" : "urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo16",
+                           }).text = '01' # siempre 01
+            
+            
+
+            #descuento por item (linea)
+            allowance_change = ET.SubElement(line, "cac:AllowanceCharge")
+            ET.SubElement(allowance_change, "cbc:ChargeIndicator").text = "false"
+            ET.SubElement(allowance_change, "cbc:AllowanceChargeReasonCode").text = "00" #00-descuentos 01-detracion 02-retencion
+            ET.SubElement(allowance_change, "cbc:Amount", currencyID = 'PEN').text = f"{descuento_item_sin_igv :.2f}"
+            ET.SubElement(allowance_change, "cbc:BaseAmount", currencyID = 'PEN').text = f"{valor_item_sin_igv :.2f}"
+
+            #igv por item
+            tax_total = ET.SubElement(line, "cac:TaxTotal")
+            ET.SubElement(tax_total,"cbc:TaxAmount", currencyID="PEN").text = f"{(valor_item_sin_igv - descuento_item_sin_igv) * factor_igv :.2f}"
+            tax_subtotal = ET.SubElement(tax_total, "cac:TaxSubtotal")
+            ET.SubElement(tax_subtotal, "cbc:TaxableAmount").text = f"{valor_item_sin_igv - descuento_item_sin_igv :.2f}"
+            ET.SubElement(tax_subtotal, "cbc:TaxAmount").text = f"{(valor_item_sin_igv - descuento_item_sin_igv) * factor_igv :.2f}"
+            tax_category = ET.SubElement(tax_subtotal, "cac:TaxCategory")
+            ET.SubElement(tax_category, "cbc:ID", 
+                          attrib={
+                                "schemeID" : "UN/ECE 5305",
+                                "schemeName" : "Tax Category Identifier",
+                                "schemeAgencyName" : "United Nations Economic Commission for Europe"
+                                }).text = "S"
+            ET.SubElement(tax_category, "cbc:Percent").text = f"{item.producto.igv * 100 :.2f}"
+            ET.SubElement(tax_category, "cbc:TaxExemptionReasonCode",
+                          attrib= {
+                                "listAgencyName" : "PE:SUNAT",
+                                "listName" : "SUNAT:Codigo de Tipo de Afectación del IGV",
+                                "listURI" : "urn:pe:gob:sunat:cpe:see:gem:catalogos:catalogo07"
+                          }).text = item.producto.codigo_afecion_igv
+            
+            area_tributo = ET.SubElement(tax_category, "cac:TaxScheme")
+            ET.SubElement(area_tributo,"cbc:ID").text = '1000'
+            ET.SubElement(area_tributo,"cbc:Name").text = 'IGV'
+            ET.SubElement(area_tributo,"cbc:TaxTypeCode").text = 'VAT'
+
+            #nombres de producto (descripcion detallada)
+            item_elem = ET.SubElement(line, "cac:Item")
+            ET.SubElement(item_elem, "cbc:Description").text = item.producto.nombre
+            
+            #valor unitario del item
+            price_elem = ET.SubElement(line, "cac:Price")
+            ET.SubElement(price_elem, "cbc:PriceAmount", currencyID="PEN").text = f"{valor_unitario_sin_igv :.2f}"
+
+        xml_bytes = ET.tostring(documento, encoding='utf-8', xml_declaration=True)
 
         response = HttpResponse(xml_bytes, content_type='application/xml')
-        response['Content-Disposition'] = f'attachment; filename=Factura_{pedido.id}.xml'
+        response['Content-Disposition'] = f'attachment; filename=Documento_{pedido.id}.xml'
         return response
 
 
