e0ada5a55b6ec4f3b7e77d3880157ece0bab50c0
yosepgx <46633004+yosepgx@users.noreply.github.com>
2025-06-08 00:33:21 -0500

    model descuento

---
+++ server/ambienta/descuentos_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DescuentosAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'descuentos_app'
---
+++ server/ambienta/descuentos_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from inventario_app.models import Producto
+class ReglaDescuento(models.Model):
+    TIPO_DESCUENTO = [
+        ('porcentaje', 'Porcentaje'),
+        ('monto_fijo', 'Monto Fijo'),
+        ('cantidad', 'Por Cantidad'),
+    ]
+    
+    producto = models.ForeignKey(Producto, on_delete=models.CASCADE, related_name="reglas")
+    fecha_inicio = models.DateField(null=False)
+    fecha_fin = models.DateField(null=False)
+    monto_fijo = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    porcentaje = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    cantidad_pagada = models.IntegerField(null=False)
+    cantidad_libre = models.IntegerField(null=False)
+    cantidad_libre_maxima = models.IntegerField(null=False)
+    tipo_descuento = models.CharField(max_choices=20, choices=TIPO_DESCUENTO)
+    activo = models.BooleanField(default=True)
